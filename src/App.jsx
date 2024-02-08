83b377b40645119d6c0b767d11af3885b9ce5523
StellaNadais <nadaisstella@gmail.com>
2024-02-08 12:01:44 -0800

    load content from external data src

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import pages from "./data/site";
 import CustomNavBar from "./CustomNavBar";
 import FruitCounter from "./FruitCounter";
 import { Container } from "react-bootstrap";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const [currentPage, setCurrentPage] = useState(pages [0]);
+  useEffect (( )=>{
+    const handleHash = ()=> {
+      //get the current hash from the url
+      const hash =window.location.hash
+    
+      //find the page from the pages array where page.href = hash url
+      const pageIndex = pages.findIndex(page => page.href == hash)
+      console.log (pageIndex)
+      //const newPage
+      setCurrentPage (pages[pageIndex])
+    }
+    window.addEventListener('hashchange', handleHash);
+    return () => {
+      window.removeEventListener('hashchange', handleHash);
+    };
+  },[])
+  
   return (
     <>
-      <CustomNavBar />
+      <CustomNavBar navItems={pages}/>
       <Container>
-        <FruitCounter />
+        <h1>{currentPage.title}</h1>
+        <p>{currentPage.content}</p>
       </Container>
 
       <p className="read-the-docs"></p>
---
+++ src/data/site.js
@@ -0,0 +1,25 @@
+const pages = [
+  {
+    href: "#home",
+    title: "Home",
+    content: "Thanks for visiting",
+  },
+
+  {
+    href: "#page1",
+    title: "Page 1",
+    content: "This is the content for page 1",
+  },
+  {
+    href: "#page2",
+    title: "Page 2",
+    content: "This is the content for page 2",
+  },
+  {
+    href: "#page3",
+    title: "Page 3",
+    content: "This is the content for page 3",
+  },
+];
+
+export default pages;
